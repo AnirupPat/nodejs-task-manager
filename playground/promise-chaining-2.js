0da26cc047b9d3a19fca58798e153b0c72ede9fe
Anirup Patnaik <anirup049@gmail.com>
2020-04-19 20:00:52 +0530

    Async/ await

--- playground/promise-chaining-2.js
+++ playground/promise-chaining-2.js
@@ -5,13 +5,33 @@
 require('../src/db/mongoose')
 const Task = require('../src/models/task')
 
-Task.findByIdAndDelete('5e9bd8653df7574894e2fd76').then((res) => {
-    console.log(res)
-    return Task.countDocuments({ completed: true })
-}).then((res) => {
+// Task.findByIdAndDelete('5e9bd8653df7574894e2fd76').then((res) => {
+//     console.log(res)
+//     return Task.countDocuments({ completed: true })
+// }).then((res) => {
+//     console.log(res)
+// }).catch((e) => {
+//     console.log(e)
+// })
+
+// async await is a small set of tool that makes work with promises easier
+
+// Use async await 
+// Create deleteTaskAndCount as an async function
+// Accept id of task to remove
+// Use await to delete task and count of incomplete tasks
+// Return the count
+// Call the function and attach then/catch to log results
+
+const deleteTaskAndCount = async (id) => {
+    const tasks = await Task.findByIdAndDelete(id)
+    console.log(tasks)
+    const count = await Task.countDocuments({completed: false})
+    return count
+}
+
+deleteTaskAndCount('5e9bd7fee472ea488b64a62b').then((res) => {
     console.log(res)
 }).catch((e) => {
     console.log(e)
 })
-
-// async await is a small set of tool that makes work with promises easier
--- playground/promise-chaining.js
+++ playground/promise-chaining.js
@@ -1,11 +1,24 @@
 require('../src/db/mongoose')
 const User = require('../src/models/user')
 
-User.findByIdAndUpdate('5e9bb77d0a92bb447e4ef0fe', { age: 0 }).then((user) => {
+// User.findByIdAndUpdate('5e9bb77d0a92bb447e4ef0fe', { age: 0 }).then((user) => {
+//     console.log(user)
+//     return User.countDocuments({ age: 0 })
+// }).then((user) => {
+//     console.log('Number of users:' +user)
+// }).catch((e) => {
+//     console.log(e)
+// })
+
+const updateAgeAndCount = async (id, age) => {
+    const user = await User.findByIdAndUpdate(id, { age })
     console.log(user)
-    return User.countDocuments({ age: 0 })
-}).then((user) => {
-    console.log('Number of users:' +user)
+    const count = await User.countDocuments({ age })
+    return count
+}
+
+updateAgeAndCount('5e9bb77d0a92bb447e4ef0fe', 0).then((res) => {
+    console.log(res)
 }).catch((e) => {
     console.log(e)
 })
